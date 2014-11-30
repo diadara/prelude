bc46d2f6fa569709b973a1bcdd5795105da352f6
Nithin Saji <nithin111@gmail.com>
2014-11-30 15:43:36 +0530

    Adding some useful keybindings
    Signed-off-by: Nithin Saji <nithin111@gmail.com>

--- personal/prelude-settings.el
+++ personal/prelude-settings.el
@@ -1,8 +1,23 @@
-(prelude-require-packages '(ido-vertical-mode guide-key))
+(prelude-require-packages '(ido-vertical-mode guide-key multiple-cursors
+                                             restclient))
 
-(ido-vertical-mode)
+;; I like vertical-ido mode
+
+(ido-vertical-mode 1)
+
+;; Still an emacs noob;
 (setq guide-key/guide-key-sequence '("C-x" "C-c" "C-r" "C-t" "C-v"))
 (setq guide-key/text-scale-amount '-1)
 (setq guide-key/recursive-key-sequence-flag t)
 (setq guide-key/popup-window-position 'bottom)
 (guide-key-mode t)
+
+;; expand region and multiple cursor work flow for fancy
+;; editing.Thanks to Emacs Rocks.
+
+(global-set-key (kbd "C-`") 'er/expand-region)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c C->") 'mc/skip-to-next-like-this)
+
+
+(scroll-bar-mode -1)
